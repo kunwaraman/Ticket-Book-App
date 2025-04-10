65f59e5fdfdeca6e12b1eccc239808a8a4006285
kunwaraman <amankunwar8472@gmail.com>
2025-04-10 23:24:39 +0530

    first

---
+++ app/src/main/java/org/example/Entity/User.java
@@ -0,0 +1,14 @@
+package org.example.Entity;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private String passowrd;
+    private String hashPassword;
+    private List<Ticket> ticketbooked;
+
+    private String userId;
+
+
+}
