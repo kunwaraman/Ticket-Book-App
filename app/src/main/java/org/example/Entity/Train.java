dfcc9e54e8911b3c7a7e6a06aa373f0e028b6ece
kunwaraman <amankunwar8472@gmail.com>
2025-04-12 23:28:20 +0530

    an

--- app/src/main/java/org/example/Entity/Train.java
+++ app/src/main/java/org/example/Entity/Train.java
@@ -1,5 +1,14 @@
 package org.example.Entity;
 
+import java.sql.Time;
+import java.util.List;
+import java.util.Map;
+
 public class Train {
-    
+    private String trainId;
+    private String trainNo;
+    private List<List<Integer>> seats;
+    private Map<String, Time> stationTimes;
+
+    private List<String> stations;
 }
--- app/src/main/java/org/example/Entity/User.java
+++ app/src/main/java/org/example/Entity/User.java
@@ -10,5 +10,51 @@ public class User {
 
     private String userId;
 
+    public User(String passowrd, String name, String hashPassword, List<Ticket> ticketbooked, String userId) {
+        this.passowrd = passowrd;
+        this.name = name;
+        this.hashPassword = hashPassword;
+        this.ticketbooked = ticketbooked;
+        this.userId = userId;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassowrd() {
+        return passowrd;
+    }
+
+    public void setPassowrd(String passowrd) {
+        this.passowrd = passowrd;
+    }
+
+    public String getHashPassword() {
+        return hashPassword;
+    }
+
+    public void setHashPassword(String hashPassword) {
+        this.hashPassword = hashPassword;
+    }
+
+    public List<Ticket> getTicketbooked() {
+        return ticketbooked;
+    }
+
+    public void setTicketbooked(List<Ticket> ticketbooked) {
+        this.ticketbooked = ticketbooked;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
